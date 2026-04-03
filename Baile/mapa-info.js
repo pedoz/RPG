d5200021855fab52137a01a1cf410670d3644ec1
Luiz Pedó <105622100+pedoz@users.noreply.github.com>
2026-04-03 19:01:15 -0300

    Add files via upload

--- Baile/mapa-info.js
+++ Baile/mapa-info.js
@@ -10,5 +10,12 @@ const infos = {
     titulo: "Termópilas",
     descricao: "Um estreito estratégico entre montanhas. Exércitos já sangraram por menos.",
     comentario: "Quem controla aqui, controla o fluxo do sul."
+  },
+  
+  Minas: {
+	  titulo: "A Fronteira Anã",
+    descricao: "Fronteira com Khaz Grudul ",
+    comentario: " Sai pra lá chorão "
   }
+
 };
